7aaefce47a5ca2a79c62a9bd8a0257c7a0ed5793
Rolf Steinecke <rolf.steinecke@mieterbund.de>
2021-07-29 09:34:46 +0200

    Neue Logdaten

--- git_push.py
+++ git_push.py
@@ -1,6 +1,6 @@
 from git import Repo
 
-PATH_OF_GIT_REPO = r'https://github.com/RolfSteinecke/mzo-auswertungen.git'  # make sure .git folder is properly configured
+PATH_OF_GIT_REPO = r'/Users/rolf/dev/Verlag/MZO_Auswertungen'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'Neue Logdaten'
 
 def git_push():
@@ -8,8 +8,10 @@ def git_push():
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(update=True)
         repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='origin')
-        origin.push()
+        #origin = repo.remote(name='origin_gh')
+        #origin.push()
+        repo.git.push('origin_gh', "HEAD:refs/for/master")
+
     except Exception as e:
         print(f'Some error occured while pushing the code {e}')    
 
